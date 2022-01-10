c1ba8ea7e30e68f2ffcb7b321cc2b1102feae84a
ProfessorQu <quint@dasookwat.com>
2022-01-10 20:12:15 +0100

    Made the
    Made the level larger.
    Added a ground material.
    Created multiple fractured cubes once you crash.

--- Car Go Vroom/Assets/Scripts/FractureAble.cs
+++ Car Go Vroom/Assets/Scripts/FractureAble.cs
@@ -10,16 +10,29 @@ public class FractureAble : MonoBehaviour
     public void Hit(Vector3 point) {
         BoxCollider collider = GetComponent<BoxCollider>();
         collider.enabled = false;
-        
-        GameObject fractured = SpawnRandomFractured(transform.position);
-        Rigidbody[] pieces = fractured.GetComponentsInChildren<Rigidbody>();
 
-        foreach (var piece in pieces) {
-            piece.AddExplosionForce(explosionForce, point, 1000f);
-        }
+        SpawnFractures(point);
 
         Destroy(gameObject);
     }
+
+    void SpawnFractures(Vector3 point) {
+        Vector3 scale = transform.lossyScale;
+        Vector3 pos = transform.position;
+        int numFractures = (int)(scale.x / 0.5);
+        float startPosX = pos.x - (scale.x - 0.5f);
+
+        for (int i = 0; i < numFractures; i++) {
+            Vector3 fracPos = new Vector3((float)(startPosX + i), pos.y, pos.z);
+            GameObject obj = SpawnRandomFractured(fracPos);
+
+            Rigidbody[] pieces = obj.GetComponentsInChildren<Rigidbody>();
+            
+            foreach(var piece in pieces) {
+                piece.AddExplosionForce(explosionForce, point, 2f);
+			}
+		}
+	}
 
     GameObject SpawnRandomFractured(Vector3 pos) {
         int idx = Random.Range(0, fracturedObjects.Length);
--- Car Go Vroom/Assets/Scripts/PlayerCollision.cs
+++ Car Go Vroom/Assets/Scripts/PlayerCollision.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     PlayerMovement movement;
+    Rigidbody rb;
 
     private void Start() {
         movement = GetComponent<PlayerMovement>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision other) {
+	private void OnCollisionEnter(Collision other) {
         if (other.collider.CompareTag("KillObstacle"))
         {
             FractureAble fract = other.collider.GetComponent<FractureAble>();
             fract.Hit(other.GetContact(0).point);
-
+            
             movement.enabled = false;
+            rb.velocity = new Vector3(0, 0, 0);
         }
     }
 }
