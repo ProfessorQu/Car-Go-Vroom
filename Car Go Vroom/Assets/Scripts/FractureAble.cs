82479e88b97239430dae8666c1de7018fae4ca73
ProfessorQu <quint@dasookwat.com>
2022-01-10 09:52:07 +0100

    Added fractures
    Added fracturable obstacles.

---
+++ Car Go Vroom/Assets/Scripts/FractureAble.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureAble : MonoBehaviour
+{
+    public GameObject[] fractured;
+    public float explosionForce = 10f;
+    public float explosionRadius = 10f;
+
+    public void Hit(Vector3 point) {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        collider.enabled = false;
+        
+        GameObject fractured = SpawnRandomFractured(transform.position);
+        Rigidbody[] pieces = fractured.GetComponentsInChildren<Rigidbody>();
+
+        foreach (var piece in pieces) {
+            piece.AddExplosionForce(explosionForce, point, explosionRadius);
+        }
+
+        Destroy(gameObject);
+    }
+
+    GameObject SpawnRandomFractured(Vector3 pos) {
+        int idx = Random.Range(0, fractured.Length);
+        return Instantiate(fractured[idx], pos, Quaternion.identity);
+    }
+}
--- Car Go Vroom/Assets/Scripts/PlayerCollision.cs
+++ Car Go Vroom/Assets/Scripts/PlayerCollision.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    PlayerMovement movement;
+
+    private void Start() {
+        movement = GetComponent<PlayerMovement>();
+    }
+
     private void OnCollisionEnter(Collision other) {
-        
+        if (other.collider.CompareTag("KillObstacle"))
+        {
+            FractureAble fract = other.collider.GetComponent<FractureAble>();
+            fract.Hit(other.GetContact(0).point);
+
+            movement.enabled = false;
+        }
     }
 }
